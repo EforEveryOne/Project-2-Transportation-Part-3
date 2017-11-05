98ef1175e043b05ae1e2c6f245afdb9ec4c4f585
EforEveryOne <31865908+EforEveryOne@users.noreply.github.com>
2017-11-05 01:11:37 -0400

    Part 3 changes.

---
+++ Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    public class CubeBehavior : MonoBehaviour {
+	public GameObject cubePrefab;
+	public int myX, myY;
+	// Both bools start false, airPlane turns on at the start if the cube is in the correct position. It is an inactive plane.
+	public bool airPlane = false;
+	//public bool activeAirPlane = false;
+
+	// When the cube is clicked it'll refer to the function in my MainScript.
+	void OnMouseDown () {
+		MainScript.ProcessClick (gameObject, myX, myY);
+	}
+}
